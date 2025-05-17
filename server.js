18a9dabc0e10dd3c357deb3056b1f16bc0ff8d67
Mohd Faisal Ansari <kzrfaisal@gmail.com>
2025-05-17 15:39:20 +0530

    [1.9] first actual query

--- server.js
+++ server.js
@@ -3,15 +3,28 @@ const { startStandaloneServer } = require('@apollo/server/standalone');
 
 // Schema
 const typeDefs = `
+  type Book {
+    id: ID!
+    title: String!
+    author: String!
+  }
+  
   type Query {
     hello: String
+    books: [Book!]!
   }
 `;
+
+const books = [
+  { id: '1', title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: '2', title: '1984', author: 'George Orwell' },
+];
 
 // Resolvers
 const resolvers = {
   Query: {
     hello: () => 'Hello, GraphQL!',
+    books: () => books,
   },
 };
 
