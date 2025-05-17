cb38876a9984036579a9158ab350ffed89182f30
Mohd Faisal Ansari <kzrfaisal@gmail.com>
2025-05-18 02:17:17 +0530

    [1.14] cleaned up and added book query

--- server.js
+++ server.js
@@ -4,56 +4,21 @@ const { startStandaloneServer } = require('@apollo/server/standalone');
 // Schema
 const typeDefs = `
   type Book {
-    id: ID!
-    title: String!
-    author: String!
-    publishedYear: Int
-    price: Float
-    inStock: Boolean
-  }
-  
-  type User {
-    id: ID!
-    name: String!
-    email: String!
+    title: String
   }
 
   type Query {
-    hello: String
-    books: [Book!]!
-    user: User
     book: Book
   }
 `;
 
-const books = [
-  {
-    id: '1',
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-    publishedYear: 1925,
-    price: 400,
-    inStock: true,
-  },
-  {
-    id: '2',
-    title: '1984',
-    author: 'George Orwell',
-    publishedYear: 1949,
-    price: 450,
-    inStock: false,
-  },
-];
-
-const user = { id: 1, name: 'John', email: 'john@xyz.com', age: 21 };
-
 // Resolvers
 const resolvers = {
   Query: {
-    hello: () => 'Hello, GraphQL!',
-    books: () => books,
-    user: () => user,
-    book: () => books[0],
+    book: () => ({
+      title: 'The Pragmatic Programmer',
+      author: 'Andy Hunt',
+    }),
   },
 };
 
