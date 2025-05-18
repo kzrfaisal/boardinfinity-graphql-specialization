a7222fe31287e465d7d316f820f43c44a50042cb
Mohd Faisal Ansari <kzrfaisal@gmail.com>
2025-05-19 02:22:18 +0530

    [1.19] overfetching example

--- server.js
+++ server.js
@@ -3,60 +3,28 @@ const { startStandaloneServer } = require('@apollo/server/standalone');
 
 // Schema
 const typeDefs = `
-   type Country {
-    code: ID!
-    name: String!
-  }
-
-  type Author {
+  type User {
     id: ID!
     name: String!
-    country: Country
-  }
-
-  type Book {
-    id: ID!
-    title: String!
-    publishedYear: Int
-    author: Author
   }
 
   type Query {
-    books: [Book]
-    book(id: ID!): Book
+    user(id: ID!): User
   }
 `;
 
 // Data
-const books = [
-  { id: '1', title: '1984', publishedYear: 1949, authorId: 'a1' },
-  { id: '2', title: 'The Hobbit', publishedYear: 1937, authorId: 'a2' },
-];
-
-const authors = [
-  { id: 'a1', name: 'George Orwell', countryCode: 'UK' },
-  { id: 'a2', name: 'J.R.R. Tolkien', countryCode: 'UK' },
-];
-
-const countries = [
-  { code: 'UK', name: 'United Kingdom' },
-  { code: 'US', name: 'United States' },
+const users = [
+  { id: '1', name: 'Sarah' },
+  { id: '2', name: 'Mike' },
 ];
 
 // Resolvers
-
 const resolvers = {
   Query: {
-    books: () => books,
-    book: (_, args) => {
-      return books.find((b) => b.id === args.id);
+    user: (_, args) => {
+      return users.find((u) => u.id === args.id);
     },
-  },
-  Book: {
-    author: (parent) => authors.find((a) => a.id === parent.authorId),
-  },
-  Author: {
-    country: (parent) => countries.find((c) => c.code === parent.countryCode),
   },
 };
 
