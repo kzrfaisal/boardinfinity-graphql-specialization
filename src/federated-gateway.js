16ba744840837cd4a398d35823364bff019a9aa4
Mohd Faisal Ansari <kzrfaisal@gmail.com>
2025-05-27 00:58:48 +0530

    [3.12] added subgraphs and federated gateway

---
+++ src/federated-gateway.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: 'users', url: 'http://localhost:4001/graphql' },
+      { name: 'address', url: 'http://localhost:4002/graphql' },
+    ],
+  }),
+});
+
+const server = new ApolloServer({ gateway });
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`🚀 Gateway ready at ${url}`);
+});
