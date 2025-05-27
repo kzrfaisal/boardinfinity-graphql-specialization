5c45eef580ccf70a47cbb5570bd5daa168493a66
Mohd Faisal Ansari <kzrfaisal@gmail.com>
2025-05-27 14:07:10 +0530

    [3.15] added debugging

--- src/federated-gateway.js
+++ src/federated-gateway.js
@@ -1,8 +1,10 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+const logger = require('./logger');
 
 const gateway = new ApolloGateway({
+  debug: true,
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       { name: 'users', url: 'http://localhost:4001/graphql' },
@@ -11,7 +13,23 @@ const gateway = new ApolloGateway({
   }),
 });
 
-const server = new ApolloServer({ gateway });
+const server = new ApolloServer({
+  gateway,
+  formatError: (err) => {
+    logger.error({
+      message: err.message,
+      code: err.extensions?.code || 'INTERNAL_SERVER_ERROR',
+      service: err.extensions?.serviceName || 'unknown',
+      path: err.path,
+      timestamp: new Date().toISOString(),
+    });
+
+    return {
+      message: err.message,
+      code: err.extensions?.code,
+    };
+  },
+});
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
